6409be9a38264a03025e35a69ed83e53461c4912
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-08-19 09:50:40 -0600

    Add files via upload

---
+++ EVA1_3_HEAP/src/eva1_3_heap/EVA1_3_HEAP.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_3_heap;
+
+/**
+ *
+ * @author DAJAHIRA NARAHI GONZALEZ ZAVALA 21550340
+ */
+public class EVA1_3_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int  x = 10;
+        Prueba Obj = new Prueba ();
+        Prueba Obj2 = new Prueba ();
+        System.out.println(x);
+        System.out.println(Obj);
+        System.out.println(Obj2);
+    }
+    
+}
+class Prueba {
+    
+}
